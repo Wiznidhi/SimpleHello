9e266c856748b3f14a4dd896935513f581638a14
ZEUSXXIV <71204882+ZEUSXXIV@users.noreply.github.com>
2021-08-23 19:30:40 +0530

    Update SimpleHello.java

--- SimpleHello.java
+++ SimpleHello.java
@@ -3,7 +3,7 @@ package SimpleHello;
 public class SimpleHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello!");
+		System.out.println("PRAHARSH FODO!");
 	}
 
 }
