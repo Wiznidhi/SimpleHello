3485108e2aef629ffe361d1a1ac97ad863366e9c
ZEUSXXIV <71204882+ZEUSXXIV@users.noreply.github.com>
2021-09-22 18:19:07 +0530

    commit1

--- SimpleHello.java
+++ SimpleHello.java
@@ -4,6 +4,8 @@ public class SimpleHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Don't Fuck you!");
+		System.out.println("jihad");
+		
 	}
 
 }
